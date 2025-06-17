0327b296db27bff9f97ebdea374d6fecf1910424
Martin <hello@northrook.com>
2025-06-17 08:13:38 +0100

    mend

--- src/Container/Compiler/CompilerPass.php
+++ src/Container/Compiler/CompilerPass.php
@@ -5,42 +5,53 @@ declare(strict_types=1);
 namespace Core\Contracts\Container\Compiler;
 
 use Attribute;
+use Core\Contracts\Container\CompilerInterface;
 use InvalidArgumentException;
 
 /**
- * @template T of CompilerPassInterface
+ * @template T of \Core\Contracts\Container\CompilerInterface
  */
 #[Attribute( Attribute::TARGET_CLASS )]
 final readonly class CompilerPass
 {
     /** @var class-string<T> */
-    public string $compilerPass;
+    public string $class;
+
+    public ?CompilerInterface $instance;
 
     /**
-     * @param null|int                 $priority  lower executed first
-     * @param CompilerPassInterface::* $pass
-     * @param array<array-key, mixed>  $arguments
+     * @param null|int                $priority  lower executed first
+     * @param CompilerInterface::*    $pass
+     * @param array<array-key, mixed> $arguments
      */
     public function __construct(
         public ?int   $priority = null,
-        public string $pass = CompilerPassInterface::PARSE,
+        public string $pass = CompilerInterface::PARSE,
         public array  $arguments = [],
     ) {}
+
+    public function __invoke() : CompilerInterface
+    {
+        return $this->instance ?? new $this->class( ...$this->arguments );
+    }
 
     /**
      * @internal called by the {@see CompilerInterface}
      *
-     * @param class-string<T> $class
+     * @param class-string<T>    $class
+     * @param ?CompilerInterface $compiler
      *
      * @return self<T>
      */
-    final public function register( string $class ) : self
+    final public function register( string $class, ?CompilerInterface $compiler = null ) : self
     {
-        $this->compilerPass = \class_exists( $class )
+        $this->class = \class_exists( $class )
                 ? $class
                 : throw new InvalidArgumentException(
                     $this::class." cannot register '{$class}', it does not exist.",
                 );
+
+        $this->instance = $compiler;
 
         return $this;
     }
--- src/Container/CompilerInterface.php
+++ src/Container/CompilerInterface.php
@@ -2,4 +2,33 @@
 
 namespace Core\Contracts\Container;
 
-interface CompilerInterface {}
+interface CompilerInterface
+{
+    /**
+     * # 1
+     * First pass.
+     *  - Resolve {@see ConfigInterface}s
+     * - {@see Autodiscover} services
+     * - {@see Autowire} dependencies
+     */
+    public const string DISCOVERY = 'compiler.discovery';
+
+    /**
+     * # 2
+     * Modify discovered {@see CompilerInterface} arguments
+     */
+    public const string PARSE = 'compiler.parse';
+
+    /** # 3
+     * Normalize {@see Parameters} by context
+     */
+    public const string OPTIMIZE = 'compiler.optimize';
+
+    /**
+     * # 4
+     * Final pass
+     * - Validating {@see ConfigInterface} values
+     * - Ensures required {@see Services} and {@see Parameters} are set
+     */
+    public const string VALIDATE = 'compiler.validate';
+}
