f4fac24bdfc618916e944ebf901d1f3e7abc9220
Martin Nielsen <mn@northrook.com>
2026-08-31 18:36:40 +0100

    mend

--- php/contracts/kernel/Http/Request/Scheme.php
+++ php/contracts/kernel/Http/Request/Scheme.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types = 1 );
 
 namespace Northrook\Http\Request;
 
@@ -8,37 +8,38 @@ use Northrook\Http\RequestInterface;
 use Northrook\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
-enum Scheme
+enum Scheme : string
 {
-    case HTTP;
-    case HTTPS;
+    case HTTP = 'http';
+    case HTTPS = 'https';
 
-    public static function from(
+    public static function resolve(
         mixed $value,
-    ): self {
-        $scheme = match (true) {
-            \is_string($value) => match (\strtolower($value)) {
+    ) : self
+    {
+        $scheme = match ( true ) {
+            \is_string( $value )               => match ( \strtolower( $value ) ) {
                 'http'  => self::HTTP,
                 'https' => self::HTTPS,
                 default => null,
             },
-            $value instanceof SymfonyRequest => $value->isSecure()
+            $value instanceof SymfonyRequest   => $value->isSecure()
                 ? self::HTTPS
                 : self::HTTP,
             $value instanceof RequestInterface => $value->scheme,
-            $value instanceof self => $value,
-            default => null,
+            $value instanceof self             => $value,
+            default                            => null,
         };
 
-        if ($scheme) {
+        if ( $scheme ) {
             return $scheme;
         }
         throw new InvalidArgumentException(
-            message: 'Unable to determine scheme from value ' . \debug_value_type($value),
+            message : 'Unable to determine scheme from value ' . \debug_value_type( $value ),
         );
     }
 
-    public function isSecure(): bool
+    public function isSecure() : bool
     {
         return $this === self::HTTPS;
     }
